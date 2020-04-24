064e8bc3bc2bfbee74bfcac4b6ad5ba12c6bc67b
amanjadon54 <amanjadon54@gmail.com>
2020-04-24 20:34:54 +0530

    Core: port changed and exception testing removed

--- src/main/java/com/centime/hello/controller/HelloController.java
+++ src/main/java/com/centime/hello/controller/HelloController.java
@@ -1,7 +1,5 @@
 package com.centime.hello.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +11,8 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @GetMapping("/hello")
     public Object hello(@RequestHeader Map<String, String> headers, String logId) {
-        logger.info("hello controller called with logID: {}", logId);
-        throw new RuntimeException();
-//        return new ResponseEntity<String>("Hello", HttpStatus.OK);
+        return new ResponseEntity<String>("Hello", HttpStatus.OK);
     }
 }
